752e83889b9f4c1561335bc264e9bcf94c3cb28d
Abhinavan2004 <abhinavanpan@gmail.com>
2026-03-10 15:45:09 +0530

    Added frontend changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import endpoints from '../data/endpoints.json';
 import { mockGraphData, mockSearchResults, mockRecommendations, mockTrends } from '../data/mockData';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'http://localhost:8000';
 
 const api = axios.create({
     baseURL: BASE_URL,
